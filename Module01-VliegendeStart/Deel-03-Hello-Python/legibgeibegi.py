0fc542757cce2ca3c8f0b9a31ffe066b3b38aade
NAWFALKOUZ <99071376@mydavinci.nl>
2022-11-03 11:04:15 +0100

    mappenstructuur

---
+++ Module01-VliegendeStart/Deel-03-Hello-Python/legibgeibegi.py
@@ -0,0 +1,9 @@
+print("Hello wereld van...")
+print("pppp    y     y   ttttttt   h   h    oooo     nnnn")
+print("p   p    y   y       t      h   h   o     o   n   n")
+print("p   p     y y        t      h   h   o     o   n   n")
+print("pppp       y         t      hhhhh   o     o   n   n")
+print("p          y         t      h   h   o     o   n   n")
+print("p          y         t      h   h   o     o   n   n")
+print("p          y         t      h   h    oooo     n   n") 
+#comments Nawfal 99701376
