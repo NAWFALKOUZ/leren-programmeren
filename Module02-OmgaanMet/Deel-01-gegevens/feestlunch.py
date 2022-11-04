936540cded3e550962799dec11f926a1971ffc26
NAWFALKOUZ <99071376@mydavinci.nl>
2022-11-04 09:56:36 +0100

    feestlunch.py

---
+++ Module02-OmgaanMet/Deel-01-gegevens/feestlunch.py
@@ -0,0 +1,18 @@
+croissantje = 0.39
+stokbrood = 2.78
+kortingsbon = 0.50
+
+aantal_croissantjes = int(input('hoeveel croissantje wil je?'))
+aantal_stokbrood = int(input("hoeveel stokbroden wil je?"))
+aantal_kortingbon = int(input("hoeveel kortingsbonnen heb je?"))
+
+
+
+
+totaal_prijs_croissant = aantal_croissantjes * croissantje 
+totaal_prijs_stokbrood = aantal_stokbrood * stokbrood
+totaal_prijs_kortingbon = aantal_kortingbon * kortingsbon
+totaal_van_alles = totaal_prijs_croissant  + totaal_prijs_stokbrood - totaal_prijs_kortingbon
+
+
+print(totaal_van_alles)
