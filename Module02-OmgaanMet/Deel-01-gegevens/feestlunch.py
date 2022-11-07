9ad984f5e4896642726c5582c54f0cbeb4d20089
NAWFALKOUZ <99071376@mydavinci.nl>
2022-11-07 14:15:09 +0100

    person.py

--- Module02-OmgaanMet/Deel-01-gegevens/feestlunch.py
+++ Module02-OmgaanMet/Deel-01-gegevens/feestlunch.py
@@ -7,12 +7,9 @@ aantal_stokbrood = int(input("hoeveel stokbroden wil je?"))
 aantal_kortingbon = int(input("hoeveel kortingsbonnen heb je?"))
 
 
-
-
 totaal_prijs_croissant = aantal_croissantjes * croissantje 
 totaal_prijs_stokbrood = aantal_stokbrood * stokbrood
 totaal_prijs_kortingbon = aantal_kortingbon * kortingsbon
 totaal_van_alles = totaal_prijs_croissant  + totaal_prijs_stokbrood - totaal_prijs_kortingbon
 
-
-print(f'De feestlunch kost je bij de bakker {totaal_van_alles}voor de {aantal_croissantjes} croissantjes en de {aantal_stokbrood} stokbroden als de {aantal_kortingbon} kortingsbonnen nog geldig zijn!')
+print(f'De feestlunch kost je bij de bakker {totaal_van_alles} voor de {aantal_croissantjes} croissantjes en de {aantal_stokbrood} stokbroden als de {aantal_kortingbon} kortingsbonnen nog geldig zijn!')
---
+++ Module02-OmgaanMet/Deel-02-userinput/person.py
@@ -0,0 +1,13 @@
+naam = input('wat is je naam?')
+Adres = input('wat is je adres?')
+Postcode = input('wat is je postcode?')
+Woonplaats = input('wat is je woonplaats?')
+
+
+
+print("--------------------------------------------------------")
+print(f' naam : {naam}')
+print(f' adres : {Adres}')
+print(f' postcode : {Postcode}')
+print(f' woonplaats : {Woonplaats}')
+print("--------------------------------------------------------")
