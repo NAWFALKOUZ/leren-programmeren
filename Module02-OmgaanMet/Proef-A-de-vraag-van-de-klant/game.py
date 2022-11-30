78d2a589a66b81027be8b63f11ebda97d219db5f
NAWFALKOUZ <99071376@mydavinci.nl>
2022-11-30 23:11:39 +0100

    game.py

--- Module02-OmgaanMet/Proef-A-de-vraag-van-de-klant/game.py
+++ Module02-OmgaanMet/Proef-A-de-vraag-van-de-klant/game.py
@@ -1,26 +1,31 @@
 print('Jij bent een DSI-agent. jouw missie is je gaat een huis binnen stormen en de twee vijanden verslaan en de gijzelaar te redden')
 
-print('Je trapt de deur in maar de deur is geboobytrapped')
-optie = input('Je verliest je wapen door de expolsie. A: pak de wapen niet of B: pak je de wapen op')
-if optie == 'a' or optie == 'A':
-    print('Je bent dood omdat je zonder wapen het huis in ging. GAME OVER')
-    exit()
-elif optie == 'b' or optie == 'B':
-    print('Je pakt je wapen op en rent het huis in')
-boven_beneden = input('Je hoort voetstappen maar je weet niet of het boven of beneden is. A: ga je naar de kelder of B: naar boven')
-if boven_beneden == 'a' or boven_beneden == 'A':
-    print('Je ziet explosieven maar nog steeds loop je naar beneden je raakt een draadje en de huis blaast op. GAME OVER')
+point_if_entry = input('Ga jij de voor of achter deur doorbreken?')
+if point_if_entry == 'achterdeur' or point_if_entry == 'voordeur':
+    print('Je breekt de deur open door en expolsie verlies je je wapen.')
+wel_of_niet = input('A: Pak je wapen op of B: Pak je hem niet op.')
+if wel_of_niet == 'a' or wel_of_niet == 'A':
+    print('Je pakt je wapen op en gaat naar binnen. je loopt naar de gang')
+elif wel_of_niet == 'b' or wel_of_niet == 'B':
+    print('Je ging zonder wapen naar binnen. Je bent dood')
     quit()
-elif boven_beneden == 'b' or boven_beneden == 'B':
-    print('Je loopt naar boven je ziet iemand. Je schiet op hem. Jij rent naar een kamer jij loopt er naar toe.')
-vraag = input('Je bent bij de deur is hoort gehuil wat doe je A: je blijft wachten of B: je schreeuwd doe je handen om hoog en gaat naar binnen')
-if vraag == 'a' or vraag == 'A':
-    print('Je hoort hem nog steeds huilen doordat kon je de ander gijzelaar niet horen.Je ziet hem in je oog hoek maar het is te laat. GAME OVER ')
+woonkamer = input('Je loopt naar de woonkamer. je ziet een van de gijzelaars en de gijzelaar. Hij schiet op jou, ga je A: Hem arresteren of B: schiet op hem ')
+if woonkamer == 'a' or woonkamer == 'A':
+    print('Hij schiet nog een keer op je want schreeuwen helpt niet (GAME OVER)')
     quit()
-elif vraag == 'b' or vraag == 'B':
-    print('Hij geeft zich over maar verteld niet waar zijn vriend is.')
-geluid = input('Je hoort geluiden in de kamer er naast. Wat doe je A:Blaas je de muur op B:Je gaat de gang in je ziet hem door het raam heen springen ')
-if geluid == 'a' or geluid == 'A':
-    print('Je stond te dicht bij de explosief dus je vloog naar achter je verloor van bewustzijn en was de verdachten weg.')
-elif geluid == 'b' or geluid == 'b':
-    print('Jij spring er achter aan een je doet een 360 no scope across the map en je raakt hem GG EZ (WIN) ')
+elif woonkamer == 'b' or woonkamer == 'B':
+    print('Je raakt hem in zijn schouder hij ligt op de grond en je red de gijzelaar. Je loopt naar boven en gaat 1 kamer steeds doorzoeken')
+sniper = input('Je ziet wat glinsteren in de kast je loopt er naar toe, het is een sniper. Pak je op of niet')
+if sniper == 'pak de sniper op' or sniper == 'ja':
+    print('je hebt de sniper op gepakt en gaat terug naar de gang')
+elif sniper == 'niet' or sniper == 'nee': 
+    print('je pakt hem niet op, je loopt terug naar de gang')
+tweede_kamer = input('Je hoort geluid van de tweede kamer, je trapt de deur je ziet de gijzelaar hij sprint uit het raam. Spring je of niet?')
+if tweede_kamer == 'niet':
+    print('omdat je niet sprong is hij er vandoor gegaan jij loser')
+    quit()
+elif tweede_kamer == 'spring':
+    if sniper == 'ja' or sniper == 'pak de sniper op':
+        print('Je springt uit de raam en doet een 360 no scope across the map GG EZ (WIN)')
+    elif sniper == 'nee' or sniper == 'niet':
+        print('Je springt uit het raam maar je kan geen 360 no scope doen want je hebt geen sniper (fail)')
