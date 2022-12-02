97e2f113d9f66e3cca8be3593b74e446cc2b152d
NAWFALKOUZ <99071376@mydavinci.nl>
2022-12-02 14:49:21 +0100

    alleen.py

---
+++ Module03-Keeropkeer/Deel-01-Voor-alles-in-de-voorraad/tijd.py
@@ -0,0 +1,11 @@
+x = 1
+i = 1
+
+while x< 13:
+  print(x,"am")
+  x += 1
+
+x = 1
+while x < 13:
+  print(x,"pm")
+  x += 1
---
+++ uitleg/zip.py
@@ -0,0 +1,7 @@
+mijn_lijst = ['anton', 'berat','cors','dirk','eduard']
+leeftijd = [16, 17, 12, 29, 15]
+
+
+for m, l in zip(mijn_lijst, leeftijd):
+    print(f'hallo, {m} je bent {l} oud')
+    
