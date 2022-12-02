8c668f243e5865cc69f43e864079cf231d727163
NAWFALKOUZ <99071376@mydavinci.nl>
2022-12-02 08:55:42 +0100

    module 3

---
+++ Module03-Keeropkeer/Deel-01-Voor-alles-in-de-voorraad/blastoff.py
@@ -0,0 +1,3 @@
+print('lancering in')
+for x in range(30, 0, -1):
+    print(x)
---
+++ Module03-Keeropkeer/Deel-01-Voor-alles-in-de-voorraad/tafel.py
@@ -0,0 +1,4 @@
+cijfer = int(input('noem een cijfer'))
+
+for x in range(1, 11):
+    print(x,'x',cijfer,'=',x*cijfer)
