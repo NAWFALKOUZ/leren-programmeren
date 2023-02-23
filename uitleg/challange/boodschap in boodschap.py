2dc7ed36c98bc14c582d3e9db887665795f0b0f5
next-ZW <99071376@mydavinci.nl>
2023-02-23 13:27:14 +0100

    Update boodschap in boodschap.py

--- uitleg/challange/boodschap in boodschap.py
+++ uitleg/challange/boodschap in boodschap.py
@@ -1,12 +1,15 @@
 def boodschap(tekst):
+    resultaat = ""
+    haakjes_geopend = False
+    for x in range(len(tekst)):
+        if tekst[x] == "[":
+            haakjes_geopend = True
+        elif tekst[x] == "]":
+            haakjes_geopend = False
+        elif haakjes_geopend:
+            resultaat += tekst[x]
+    return resultaat
 
-    
-
-tekst = """En ze stu[re]n [i]ngekleurde prentbriefkaarten van plekken waarvan ze zich niet reali[s]eren dat ze er nooit geweest zijn 
-[a]an ' Iedereen op nummer 22, weer is prachti[g], onz[e] kamer is aa[n]gekruisd. 
-Missen jullie. E[t]en[ ]i[s] vettig , maar we hebben een geweldig leuk restaurantje gevonden in de achterstraatjes waar ze Heine[ke]n 
-hebben en kaas en uien chips en iemand die "Een beetje verliefd" speel[t] op een a[c]cordeon ' 
-en je zit vier dagen vast op Schip[h]ol voor je vijfdaagse vliegvakantie met niks anders te eten dan uitgedroogde voorverpakte 
-boterhammen..."""
+tekst = """En ze stu[re]n [i]ngekleurde prentbriefkaarten van plekken waarvan ze zich niet reali[s]eren dat ze er nooit geweest zijn [a]an ' Iedereen op nummer 22, weer is prachti[g], onz[e] kamer is aa[n]gekruisd. Missen jullie. E[t]en[ ]i[s] vettig , maar we hebben een geweldig leuk restaurantje gevonden in de achterstraatjes waar ze Heine[ke]n hebben en kaas en uien chips en iemand die "Een beetje verliefd" speel[t] op een a[c]cordeon ' en je zit vier dagen vast op Schip[h]ol voor je vijfdaagse vliegvakantie met niks anders te eten dan uitgedroogde voorverpakte boterhammen..."""
 
 print(boodschap(tekst))
