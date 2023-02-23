a97f44c6852f8b5bdd3057078423b7c3fa1df67a
next-ZW <99071376@mydavinci.nl>
2023-02-23 09:08:16 +0100

    deck

---
+++ Module04-Lijstje-en-samenstellingen/Deel-01-Collections/M04.D01.O3/deck.py
@@ -0,0 +1,22 @@
+import random
+kleuren = ['harten', 'klaveren', 'schoppen', 'ruiten']
+kaarten = ['boer','vrouw','heer','aas']
+jokers = ['Joker 1','joker 2']
+lijst = []
+
+for x in kleuren:
+    for y in kaarten:
+        lijst.append(f"{x} {y}")
+    for z in range(1,11):
+        lijst.append(f"{x} {z}")
+for a in jokers:
+    lijst.append(a)
+
+random.shuffle(lijst)
+
+for b in range(1,8):
+    kaart = random.choice(lijst)
+    print(f'kaart {b}: {kaart}')
+    lijst.remove(kaart)
+
+print(f'de rest van de kaarten is (47):{lijst}')
