95d3c7d819e082fe870d7702778dd5a850fbd022
next-ZW <99071376@mydavinci.nl>
2023-03-16 12:10:29 +0100

    py

--- Module04-Lijstje-en-samenstellingen/Deel-01-Collections/M04.D01.O1/Dagen.py
+++ Module04-Lijstje-en-samenstellingen/Deel-01-Collections/M04.D01.O1/Dagen.py
@@ -1,16 +1,23 @@
 Dagen = ("Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag")
+#naast elkaar geprint worden
 
 print(f"Alle dagen van de week zijn {Dagen}")
 
+print(f'de weekenddagen zijn: ')
 for y in range(5, 7): #range(start, stop, step) 
-    print(f'de weekenddagen zijn: {Dagen[y]}')
+    print(Dagen[y], end=" ")
+print("\n")
 
+print(f'Alle dagen van de week in omgekeerde volgorde zijn:')
 for z in range(6, -1, -1):
-    print(z)
-    print(f'Alle dagen van de week in omgekeerde volgorde zijn: {Dagen[z]}')
+    print(Dagen[z], end =" ")
+print("\n")
 
+print(f'De werkdagen in omgekeerde volgorde zijn:')
 for a in range(4,  -1, -1):
-    print(f'De werkdagen in omgekeerde volgorde zijn: {Dagen[a]}')
+    print(Dagen[a], end=" ")
+print("\n")
 
+print('De weekenddagen in omgekeerde volgorde zijn:')
 for b in range(6,  4, -1):
-    print(f'De weekenddagen in omgekeerde volgorde zijn: {Dagen[b]}')
+    print(Dagen[b], end=" ")
--- Module04-Lijstje-en-samenstellingen/Deel-01-Collections/M04.D01.O3/deck.py
+++ Module04-Lijstje-en-samenstellingen/Deel-01-Collections/M04.D01.O3/deck.py
@@ -2,9 +2,8 @@ import random
 
 
 kleuren = ('harten', 'klaveren', 'schoppen', 'ruiten')
-kaarten = ('boer','vrouw','heer','aas')
-jokers = ('Joker 1','joker 2')
-
+kaarten = ('boer', 'vrouw', 'heer', 'aas')
+jokers = ('Joker 1', 'joker 2')
 
 lijst = []
 
@@ -13,19 +12,19 @@ for x in kleuren:
     for y in kaarten:
         lijst.append(f"{x} {y}")
 
-    for z in range(2,11):
+    for z in range(2, 11):
         lijst.append(f"{x} {z}")
+    
 for a in jokers:
     lijst.append(a)
 
-
 random.shuffle(lijst)
-
+print(lijst)
 
 for b in range(1,8):
-    kaart = random.choice(lijst)
-    print(f'kaart {b}: {kaart}')
+    kaart = lijst[b]
+    print(f"kaart {b}: {kaart}")
     lijst.remove(kaart)
 
 
-print(f'de rest van de kaarten is {len(lijst)}:{lijst}')
+# print(f'de rest van de kaarten is {len(lijst)}:{lijst}')
