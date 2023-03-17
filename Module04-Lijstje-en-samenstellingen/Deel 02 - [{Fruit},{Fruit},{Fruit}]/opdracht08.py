683789e314c61f2525a4db1ba5d06913f24369da
next-ZW <99071376@mydavinci.nl>
2023-03-17 10:30:00 +0100

    fruitmand

---
+++ Module04-Lijstje-en-samenstellingen/Deel 02 - [{Fruit},{Fruit},{Fruit}]/opdracht08.py
@@ -0,0 +1,6 @@
+from fruitmand import fruitmand
+
+fruitmand.append({'name': 'watermeloen', 'weight': 1600, 'color': 'green', 'round': True})
+
+for x in fruitmand:
+    print(x['weight'])
---
+++ Module04-Lijstje-en-samenstellingen/Deel 02 - [{Fruit},{Fruit},{Fruit}]/opdracht09.py
@@ -0,0 +1,6 @@
+from fruitmand import fruitmand
+
+fruitmand.pop(4)
+
+for x in fruitmand:
+    print(x['color'])
