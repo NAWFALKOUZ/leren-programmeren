fa426f5b97748ca0890b8b999872157ec552b86f
next-ZW <99071376@mydavinci.nl>
2023-03-17 09:30:45 +0100

    fruitmand

--- Module04-Lijstje-en-samenstellingen/Deel 02 - [{Fruit},{Fruit},{Fruit}]/opdracht04.py
+++ Module04-Lijstje-en-samenstellingen/Deel 02 - [{Fruit},{Fruit},{Fruit}]/opdracht04.py
@@ -1,7 +1,7 @@
 from fruitmand import fruitmand
 import random
 
-hoeveel = input("type een nummer")
+hoeveel = input("type een nummer: ")
 
 for x in (hoeveel):
     keuzen = random.randint(0,(len(fruitmand)))
