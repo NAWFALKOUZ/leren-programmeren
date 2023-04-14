7725797f59c410550f283311e3a1738d7aac9d3b
next-ZW <99071376@mydavinci.nl>
2023-04-14 09:22:58 +0200

    Update Lootjes verdeler.py

--- Module04-Lijstje-en-samenstellingen/Proef A - Lootjes verdeler/Lootjes verdeler.py
+++ Module04-Lijstje-en-samenstellingen/Proef A - Lootjes verdeler/Lootjes verdeler.py
@@ -3,6 +3,7 @@
 # Als er minder dan 5 zijn vraagt hij het opnieuw en als er 3 namen zijn vraagt hij steeds of je nog meer namen wilt invullen 
 # trek random lootjes zonder dat iemand zichzelf heeft
 # Print de lijst van namen en bijbehorende lootjes
+
 import random
 
 namen = []
