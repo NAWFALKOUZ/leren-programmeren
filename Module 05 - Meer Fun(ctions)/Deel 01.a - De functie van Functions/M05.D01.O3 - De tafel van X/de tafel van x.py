58ddfd1b17d5f57362a039d804fd57b398830dbd
next-ZW <99071376@mydavinci.nl>
2023-05-10 16:29:29 +0200

    py

---
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O3 - De tafel van X/de tafel van x.py
@@ -0,0 +1,5 @@
+def tafel(aantal):
+    for x in range(1,11):
+        print(x,'x',aantal,'=',x*aantal)
+aantal = int(input("Welke tafel wil je leren?"))
+tafel(aantal)
---
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O4 - Namen en leeftijden/Namen en leeftijden.py
@@ -0,0 +1,8 @@
+name_age_list = []
+
+def name_age():
+    name_age_dict = {}
+    naam = input("Wat is je naam? ")
+    leeftijd = input("Hoe oud ben je? ")
+    name_age_dict['name'] = naam
+    name_age_dict['age'] = leeftijd
