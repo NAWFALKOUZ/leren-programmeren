1d32f0ff14cc0a20ba992d6cf7f6451aae060770
next-ZW <99071376@mydavinci.nl>
2023-05-11 09:48:10 +0200

    py

--- Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O4 - Namen en leeftijden/Namen en leeftijden.py
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O4 - Namen en leeftijden/Namen en leeftijden.py
@@ -3,6 +3,15 @@ name_age_list = []
 def name_age():
     name_age_dict = {}
     naam = input("Wat is je naam? ")
-    leeftijd = input("Hoe oud ben je? ")
+    leeftijd = int(input("Hoe oud ben je? "))
     name_age_dict['name'] = naam
     name_age_dict['age'] = leeftijd
+    return name_age_dict
+
+while True:
+    name_age_list.append(name_age())
+    if input("wil nog een naam toevoegen (ja/nee): ") == "nee":
+        break
+
+for x in name_age_list:
+    print(f"{x['name']} is {x['age']} jaar")
