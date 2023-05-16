d5e1896c91cab4783609264baee2a077c91a02b8
next-ZW <99071376@mydavinci.nl>
2023-05-16 21:04:54 +0200

    py

---
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O5 - Fibonacci reeks/fibonacci.py
@@ -0,0 +1,7 @@
+def fibonacci(aantal: int) -> list:
+    getallenreeks = [0,1]
+    for x in range(aantal):
+        getallenreeks.append(getallenreeks[x] + getallenreeks[x + 1])
+    snede = getallenreeks[-1] / getallenreeks[-2]
+    return getallenreeks, snede
+print(fibonacci(10))
---
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
@@ -0,0 +1,21 @@
+choice = input('wat wilt u doen?\nA) getallen optellen\nB) getallen aftrekken\nC) getallen vermenigvuldigen\nD) getallen delen\nE) getal ophogen\nF) getal verlagen\nG) getal verdubbelen\nH) getal halveren\nKies: ')
+
+
+
+def addition(nummer1,nummer2):
+    nummer1 + nummer2
+    return 0 
+
+def subtraction(nummer1,nummer2):
+    nummer1 - nummer2
+    return 0
+
+def multiplication(nummer1,nummer2):
+    nummer1 * nummer2
+    return 0
+
+def division(nummer1,nummer2):
+    nummer1 / nummer2
+    return 0
+
+
