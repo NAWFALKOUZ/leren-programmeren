2665ca15ef76c3c374305f76b3de6787424be7c7
next-ZW <99071376@mydavinci.nl>
2023-05-22 07:58:45 +0200

    Update calculator.py

--- Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
@@ -1,21 +1,17 @@
-choice = input('wat wilt u doen?\nA) getallen optellen\nB) getallen aftrekken\nC) getallen vermenigvuldigen\nD) getallen delen\nE) getal ophogen\nF) getal verlagen\nG) getal verdubbelen\nH) getal halveren\nKies: ')
+choice = input('wat wilt u doen?\nA) getallen optellen\nB) getallen aftrekken\nC) getallen vermenigvuldigen\nD) getallen delen\nE) getal ophogen\nF) getal verlagen\nG) getal verdubbelen\nH) getal halveren\nKies: ').upper()
 
 
 
 def addition(nummer1,nummer2):
-    nummer1 + nummer2
-    return 0 
+    return nummer1 + nummer2
 
 def subtraction(nummer1,nummer2):
-    nummer1 - nummer2
-    return 0
+    return nummer1 - nummer2
+
 
 def multiplication(nummer1,nummer2):
-    nummer1 * nummer2
-    return 0
+    return nummer1 * nummer2
 
 def division(nummer1,nummer2):
-    nummer1 / nummer2
-    return 0
-
+    return nummer1 / nummer2
 
