07d8493ec36292d181c636c37872f2fa6fd8a106
next-ZW <99071376@mydavinci.nl>
2023-05-25 01:22:59 +0200

    Update calculator.py

--- Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
@@ -15,3 +15,27 @@ def multiplication(nummer1,nummer2):
 def division(nummer1,nummer2):
     return nummer1 / nummer2
 
+
+  if choice == 'A':
+        result = addition(nummer1, nummer2)
+        operation = "optellen"
+    elif choice == 'B':
+        result = subtraction(nummer1, nummer2)
+        operation = "aftrekken"
+    elif choice == 'C':
+        result = multiplication(nummer1, nummer2)
+    elif choice == 'D':
+        result = division(nummer1, nummer2)
+        operation = "delen"
+    elif choice == 'E':s
+        result = addition(nummer1, nummer2)
+        operation = "ophogen"
+    elif choice == 'F':
+        result = subtraction(nummer1, nummer2)
+        operation = "verlagen"
+    elif choice == 'G':
+        result = multiplication(nummer1, nummer2)
+        operation = "verdubbelen"
+    elif choice == 'H':
+        result = division(nummer1, nummer2)
+        operation = "halveren"
