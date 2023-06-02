bac661ddf4acb5070695d91e0de4bdfb8393a40c
next-ZW <99071376@mydavinci.nl>
2023-06-02 13:38:42 +0200

    Update calculator.py

--- Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
@@ -1,41 +1,65 @@
-choice = input('wat wilt u doen?\nA) getallen optellen\nB) getallen aftrekken\nC) getallen vermenigvuldigen\nD) getallen delen\nE) getal ophogen\nF) getal verlagen\nG) getal verdubbelen\nH) getal halveren\nKies: ').upper()
+def addition (number1, number2):
+    return number1 + number2
+def subtraction (number1, number2):
+    return number1 - number2
+def multiplication (number1, number2):
+    return number1 * number2
+def division (number1, number2):
+    return number1 / number2
 
+keuze = " "
+while not keuze in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'):
+    keuze = input("Wat wilt u doen? A = getallen optellen, B = getallen aftrekken, C = getallen vermenigvuldigen, D = getallen delen, E = getal ophogen, F = getal verlagen, G = getal verdubbelen of H = getal halveren?, type 'niks' als je niks wilt doen: ").lower()
+    if keuze == "niks":
+        exit()
 
+aantal = -1
+while True:
+    if aantal > -1 and keuze != 'i' and keuze in ('a', 'b', 'c', 'd'):
+        number1 = aantal
+        number2 = int(input("Geef de 2e getal op: "))
 
-def addition(nummer1,nummer2):
-    return nummer1 + nummer2
+    elif aantal > -1 and keuze != 'i' and keuze in ('e', 'f', 'g', 'h'):
+        number1 = aantal
 
-def subtraction(nummer1,nummer2):
-    return nummer1 - nummer2
+        
+    elif keuze in ('a', 'b', 'c', 'd'):
+        number1 = int(input("Geef een getal op: "))
+        number2 = int(input("Geef nog een getal op: "))
 
+    elif keuze in ('e', 'f', 'g', 'h'):
+        number1 = int(input("Geef een getal op: "))
 
-def multiplication(nummer1,nummer2):
-    return nummer1 * nummer2
+    else:
+        exit()
 
-def division(nummer1,nummer2):
-    return nummer1 / nummer2
+    if keuze == "a":
+        aantal = addition(number1, number2)
+        print(aantal)
+    elif keuze == "b":
+        aantal = subtraction(number1, number2)
+        print(aantal)
+    elif keuze == "c":
+        aantal = multiplication(number1, number2)
+        print(aantal)
+    elif keuze == "d":
+        aantal = division(number1, number2)
+        print(aantal)
+    elif keuze == "e":
+        number2 = 1
+        aantal = addition(number1, number2)
+        print(aantal)
+    elif keuze == "f":
+        number2 = 1
+        aantal = subtraction(number1, number2)
+        print(aantal)
+    elif keuze == "g":
+        number2 = 2
+        aantal = multiplication(number1, number2)
+        print(aantal)
+    else:
+        number2 = 2
+        aantal = division(number1, number2)
+        print(aantal)
 
-
-  if choice == 'A':
-        result = addition(nummer1, nummer2)
-        operation = "optellen"
-    elif choice == 'B':
-        result = subtraction(nummer1, nummer2)
-        operation = "aftrekken"
-    elif choice == 'C':
-        result = multiplication(nummer1, nummer2)
-    elif choice == 'D':
-        result = division(nummer1, nummer2)
-        operation = "delen"
-    elif choice == 'E':s
-        result = addition(nummer1, nummer2)
-        operation = "ophogen"
-    elif choice == 'F':
-        result = subtraction(nummer1, nummer2)
-        operation = "verlagen"
-    elif choice == 'G':
-        result = multiplication(nummer1, nummer2)
-        operation = "verdubbelen"
-    elif choice == 'H':
-        result = division(nummer1, nummer2)
-        operation = "halveren"
+    keuze = input(f"Wil je wat met het antwoord ({aantal}) doen? A = getallen optellen, B = getallen aftrekken, C = getallen vermenigvuldigen, D = getallen delen, E = getal ophogen, F = getal verlagen, G = getal verdubbelen of H = getal halveren of I = Nee?")
