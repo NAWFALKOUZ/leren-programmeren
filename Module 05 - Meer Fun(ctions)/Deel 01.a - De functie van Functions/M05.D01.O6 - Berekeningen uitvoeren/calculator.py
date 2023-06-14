309debce81c81d7143eca60ae4a5f051a514887c
next-ZW <99071376@mydavinci.nl>
2023-06-15 01:03:33 +0200

    Update calculator.py

--- Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
@@ -17,51 +17,53 @@ while not choice in ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'):
         exit()
 
 aantal = -1
+firstRound = True
 while True:
-    if aantal > -1 and choice in ('A', 'B', 'C', 'D'):
-        number1 = aantal
-        number2 = float(input("Geef het tweede getal op: "))
-
-    elif aantal > -1 and choice in ('E', 'F', 'G', 'H'):
-        number1 = aantal
-
-    elif choice in ('A', 'B', 'C', 'D'):
-        number1 = float(input("Geef een getal op: "))
-        number2 = float(input("Geef nog een getal op: "))
-
-    elif choice in ('E', 'F', 'G', 'H'):
-        number1 = float(input("Geef een getal op: "))
-
+    if not firstRound:
+        n1 = aantal
+        if choice in ('A', 'B', 'C', 'D'):
+            while True:
+                try:
+                    n2 = float(input("Geef het tweede getal op: "))
+                    break
+                except ValueError:
+                    print("Dit is geen geldig getal. Probeer opnieuw.")
+        else:
+            n2 = 1 if choice in ('E', 'F') else 2
     else:
-        exit()
+        firstRound = False
+        while True:
+            try:
+                n1 = float(input("Geef een getal op: "))
+                n2 = float(input("Geef nog een getal op: ")) if choice in ('A', 'B', 'C', 'D') else 1 if choice in ('E', 'F') else 2
+                break
+            except ValueError:
+                print("Dit is geen geldig getal. Probeer opnieuw.")
 
     if choice == "A":
-        aantal = addition(number1, number2)
-        print(aantal)
+        aantal = addition(n1, n2)
     elif choice == "B":
-        aantal = subtraction(number1, number2)
-        print(aantal)
+        aantal = subtraction(n1, n2)
     elif choice == "C":
-        aantal = multiplication(number1, number2)
-        print(aantal)
+        aantal = multiplication(n1, n2)
     elif choice == "D":
-        aantal = division(number1, number2)
-        print(aantal)
+        if n2 == 0:
+            print("Kan niet delen door 0")
+            continue
+        aantal = division(n1, n2)
     elif choice == "E":
-        number2 = 1
-        aantal = addition(number1, number2)
-        print(aantal)
+        aantal = addition(n1, n2)
     elif choice == "F":
-        number2 = 1
-        aantal = subtraction(number1, number2)
-        print(aantal)
+        aantal = subtraction(n1, n2)
     elif choice == "G":
-        number2 = 2
-        aantal = multiplication(number1, number2)
-        print(aantal)
+        aantal = multiplication(n1, n2)
     else:
-        number2 = 2
-        aantal = division(number1, number2)
-        print(aantal)
+        if n2 == 0:
+            print("Kan niet delen door 0")
+            continue
+        aantal = division(n1, n2)
 
-    choice = input(f'Wil je iets met het uitkomst ({aantal})?\nA) Getallen optellen\nB) Getallen aftrekken\nC) Getallen vermenigvuldigen\nD) Getallen delen\nE) Getal ophogen\nF) Getal verlagen\nG) Getal verdubbelen\nH) Getal halveren\nKies: ').upper()
+    print(aantal)
+
+    choice = input(f'Wil je iets met het uitkomst ({aantal}) doen?\nA) Getallen optellen\nB) Getallen aftrekken\nC) Getallen vermenigvuldigen\nD) Getallen delen\nE) Getal ophogen\nF) Getal verlagen\nG) Getal verdubbelen\nH) Getal halveren\nKies: ').upper()
+
