ce933fa95f3530e2a3baa590f7a0ef27867938f9
next-ZW <99071376@mydavinci.nl>
2023-11-27 09:49:46 +0100

    Update calculator.py

--- Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
+++ Module 05 - Meer Fun(ctions)/Deel 01.a - De functie van Functions/M05.D01.O6 - Berekeningen uitvoeren/calculator.py
@@ -9,74 +9,62 @@ def multiplication(number1, number2):
 
 def division(number1, number2):
     if number2 == 0:
-        raise ValueError("kan niet delen door 0")
+        print("Kan niet delen door 0")
+        return None
     return number1 / number2
 
 choice = " "
-while not choice in ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'):
+aantal = None  
+
+while choice not in ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'):
     choice = input('Wat wilt u doen?\nA) Getallen optellen\nB) Getallen aftrekken\nC) Getallen vermenigvuldigen\nD) Getallen delen\nE) Getal ophogen\nF) Getal verlagen\nG) Getal verdubbelen\nH) Getal halveren\nKies: ').upper()
-
-aantal = -1
+    
+    
 while True:
-    if aantal > -1 and choice in ('A', 'B', 'C', 'D'):
+    if aantal is not None:
         number1 = aantal
-        while True:
-            try:
-                number2 = float(input("Geef het tweede getal op: "))
-                break
-            except ValueError:
-                print("Dit is geen geldig getal.")
-
-    elif aantal > -1 and choice in ('E', 'F', 'G', 'H'):
-        number1 = aantal
-
-    elif choice in ('A', 'B', 'C', 'D'):
-        while True:
-            try:
-                number1 = float(input("Geef een getal op: "))
-                number2 = float(input("Geef nog een getal op: "))
-                break
-            except ValueError:
-                print("Dit is geen geldig getal.")
-
-    elif choice in ('E', 'F', 'G', 'H'):
-        while True:
-            try:
-                number1 = float(input("Geef een getal op: "))
-                break
-            except ValueError:
-                print("Dit is geen geldig getal.")
-
     else:
-        exit()
+        if choice in ('A', 'B', 'C', 'D'):
+            while True:
+                try:
+                    number1 = float(input("Geef het eerste getal op: "))
+                    break
+                except ValueError:
+                    print("Dit is geen geldig getal.")
+        elif choice in ('E', 'F', 'G', 'H'):
+            while True:
+                try:
+                    number1 = float(input("Geef een getal op: "))
+                    break
+                except ValueError:
+                    print("Dit is geen geldig getal.")
+        else:
+            exit()
 
     if choice == "A":
+        number2 = float(input("Geef het tweede getal op: "))
         aantal = addition(number1, number2)
     elif choice == "B":
+        number2 = float(input("Geef het tweede getal op: "))
         aantal = subtraction(number1, number2)
     elif choice == "C":
+        number2 = float(input("Geef het tweede getal op: "))
         aantal = multiplication(number1, number2)
     elif choice == "D":
-        try:
-            aantal = division(number1, number2)
-        except ValueError:
-            print("Kan niet delen door 0")
+        number2 = float(input("Geef het tweede getal op: "))
+        result = division(number1, number2)
+        if result is None:
             continue
+        aantal = result
     elif choice == "E":
-        number2 = 1
-        aantal = addition(number1, number2)
+        aantal = addition(number1, 1)
     elif choice == "F":
-        number2 = 1
-        aantal = subtraction(number1, number2)
+        aantal = subtraction(number1, 1)
     elif choice == "G":
-        number2 = 2
-        aantal = multiplication(number1, number2)
+        aantal = multiplication(number1, 2)
     else:
-        try:
-            number2 = 2
-            aantal = division(number1, number2)
-        except ValueError:
-            print("Kan niet delen door 0")
+        aantal = division(number1, 2)
+        if aantal is None:
             continue
 
     print(aantal)
